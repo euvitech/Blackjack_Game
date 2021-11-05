23c2b547b6df8b55ce25d256f0950a73c90f1c6f
Vinicius Pereira <vguard.2003@gmail.com>
2021-11-05 16:08:07 -0300

    add condition for result in BlackJack

--- main..js
+++ main..js
@@ -1,2 +1,12 @@
 
+let firstCard = 11
+let secondCard = 11
+let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Você quer mais uma carta?")
+} else if(sum == 21) {
+    console.log("Uhhuuu, você conseguiu 21")
+} else if(sum > 21) {
+    console.log("Puts amigo, não foi dessa vez ;-;")
+}
