04d7df4351d17106eae996bfee320111dea75659
Vinicius-Vguard <vguard.2003@gmail.com>
2021-11-08 22:29:18 -0300

    Add messages and boolean conditional

--- main..js
+++ main..js
@@ -1,12 +1,21 @@
 
-let firstCard = 11
-let secondCard = 11
+let firstCard = 1
+let secondCard = 20
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let inAlive = true
+let calledMessage
 
 if (sum < 21) {
-    console.log("Você quer mais uma carta?")
-} else if(sum == 21) {
-    console.log("Uhhuuu, você conseguiu 21")
-} else if(sum > 21) {
-    console.log("Puts amigo, não foi dessa vez ;-;")
+    calledMessage = "Você quer mais uma carta?"
+} else if(sum === 21) {
+    calledMessage = "Uhhuuu, você conseguiu 21"
+    hasBlackJack = true
+} else {
+    calledMessage = "Puts amigo, não foi dessa vez ;-;"
+    inAlive = false
 }
+
+
+console.log(calledMessage)
+
