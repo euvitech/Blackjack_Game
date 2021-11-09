61cb8c767339e737aa8254b4596d5c3b73d4bbd2
Vinicius-Vguard <vguard.2003@gmail.com>
2021-11-09 11:50:47 -0300

    add button start game and new card game

--- main..js
+++ main..js
@@ -1,21 +1,30 @@
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
 
-let firstCard = 1
-let secondCard = 20
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let inAlive = true
-let calledMessage
-
-if (sum < 21) {
-    calledMessage = "Você quer mais uma carta?"
-} else if(sum === 21) {
-    calledMessage = "Uhhuuu, você conseguiu 21"
-    hasBlackJack = true
-} else {
-    calledMessage = "Puts amigo, não foi dessa vez ;-;"
-    inAlive = false
+function startGame() {
+    let firstCard = 10
+    let secondCard = 4
+    let sum = firstCard + secondCard
+    let hasBlackJack = false
+    let inAlive = true
+    let calledMessage
+    
+    if (sum < 21) {
+        calledMessage = "Do you want one more letter??"
+    } else if(sum === 21) {
+        calledMessage = "Uhuuu, You got Blackjack"
+        hasBlackJack = true
+    } else {
+        calledMessage = "It wasn't this time"
+        inAlive = false
+    }
+    
+    messageEl.innerHTML = calledMessage
+    sumEl.innerHTML = `Sum: ${sum}`
+    cardEl.innerHTML = `Card: ${firstCard}, ${secondCard}`
 }
 
-
-console.log(calledMessage)
-
+function newCard() {
+    console.log("New Card")
+}
